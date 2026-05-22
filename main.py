54393571837777011aaccf31e26b21e9934212f3
Reda Kadri <rkadri@c3r9p14.1337.ma>
2026-05-22 05:49:06 +0100

    reading the config file

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from fly-in!")
+import sys
+
+from config_reader import ConfigReader
 
 
 if __name__ == "__main__":
-    main()
+    config_reader = ConfigReader()
+    try:
+        config = config_reader.loads()
+    except FileNotFoundError as e:
+        print(f"[ERROR]: File not found — {e.filename}")
+        sys.exit(1)
+    except PermissionError as e:
+        print(f"[ERROR]: Permission denied reading '{e.filename}'")
+        sys.exit(1)
